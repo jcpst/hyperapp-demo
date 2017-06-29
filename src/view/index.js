681d22c059eed6a9967aad75d27b5c7b910319b7
Joe Post <joseph.post@chrobinson.com>
2017-06-29 15:03:36 -0500

    prettier

--- src/view/index.js
+++ src/view/index.js
@@ -2,6 +2,6 @@ import { h } from 'hyperapp'
 import DataTable from './components/DataTable'
 
 export default (state, actions) =>
-  <main class='container'>
+  <main class="container">
     <DataTable search={actions.search} data={state.data} />
   </main>
